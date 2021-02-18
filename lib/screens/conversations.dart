bc46d929f50e32f0c95a12bf49986290ac5ace03
Shreyas Bhaktharam <bhaktharamshreyas@gmail.com>
2021-02-18 17:31:59 +0530

    Implemented search bar and app bar

---
+++ lib/screens/conversations.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Conversations extends StatefulWidget {
+  @override
+  _ConversationsState createState() => _ConversationsState();
+}
+
+class _ConversationsState extends State<Conversations> {
+  TextEditingController _searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 20, right:20, top:15),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image:NetworkImage("https://randomuser.me/api/portraits/men/17.jpg"),
+                          fit: BoxFit.cover
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Chats",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    Icons.edit,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color(0xFFe9eaec),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextField(
+                  cursorColor: Color(0xFF000000),
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Color(0xFF000000).withOpacity(0.5),
+                    ),
+                    hintText: "Search",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
